1f3fc329e4b1fb0e57162f65991263a8226e4670
Camilo Piñeros <kamilo.martinez123.km@gmail.com>
2025-09-26 17:06:19 -0500

    Actualización de estilos CSS y ajustes en vistas principales

--- resources/views/pages/cotiza.blade.php
+++ resources/views/pages/cotiza.blade.php
@@ -6,39 +6,62 @@
     <title>Cotisar</title>
     <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
 </head>
-<body>
-        <!-- Contenido principal de la página -->
-    <main>
-    <!-- Encabezado principal del sitio -->
-    <header id="barra_superior">
-        <section class="cuadro_superior">
-            <!-- Logo del sitio -->
-            <div>
-                <a href="http://127.0.0.1:8000/">
-                    <img src="{{asset('Img/Logo1-orden.png')}}" alt="Logo de fábrica" class="logo_principal">
-                </a>
-            </div>
-
-            <!-- Buscador -->
-            <div>
-                <input type="text" class="busqueda" placeholder="¿Qué producto deseas buscar?">
-            </div>
-
-            <!-- Botones de carrito y perfil -->
-            <div class="botones">
-            <a href="{{ route('carro-compra') }}" class="icono-carrito">
-            <img src="{{asset('Img/CarritoCompra.png')}}" alt="Carrito" class="icono-img">
-            </a>
-            <div class="menu-usuario">
-            <a href="#" class="icono-usuario">
-            <img src="{{asset('Img/usuario.png')}}" alt="Usuario" class="icono-img">
-            </a>
-        <div class="submenu">
-        <a href="inicio.html">Iniciar Sesión</a>
-        <a href="registro.html">Registrarse</a>
-    </div>
-    </div>
-            </div>
+    <body class="font-sans antialiased">
+        <!-- Encabezado principal del sitio -->
+        <header id="barra_superior">
+            <section class="cuadro_superior">
+                <!-- Logo del sitio -->
+                <div>
+                    <a href="http://127.0.0.1:8000/" >
+                        <img src="{{asset('Img/Logo1-orden.png')}}" class="logo_principal" alt="Logo Orden Rae" />
+                    </a>
+                </div>
+                <!-- Buscador -->
+                <div>
+                    <input 
+                        type="text" 
+                        class="busqueda"
+                        name="Buscador" 
+                        id="Buscador" 
+                        placeholder="¿Qué producto deseas buscar?" 
+                        class="w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-base text-black placeholder-gray-500 shadow-sm focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-200 dark:text-white dark:placeholder-gray-400"
+                    />
+                </div>
+                    <!-- Botones de carrito y perfil -->
+                <div class="botones">
+                    <a href="{{ route('carro-compra') }}" class="icono-carrito">
+                        <img src="{{asset('Img/CarritoCompra.png')}}" alt="Carrito" class="icono-img">
+                    </a>
+                    <div class="menu-usuario">
+                        <a href="#" class="icono-usuario">
+                            <img src="{{asset('Img/usuario.png')}}" alt="Usuario" class="icono-img">
+                        </a>
+                        @if (Route::has('login'))
+                            <div class="submenu">
+                                    @auth
+                                            <a
+                                                href="{{ url('/dashboard') }}"                                            
+                                            >
+                                                Dashboard
+                                            </a>
+                                    @else
+                                            <a
+                                                href="{{ route('login') }}"
+                                            >
+                                                Iniciar sesión
+                                            </a>
+                                    @if (Route::has('register'))
+                                            <a
+                                                href="{{ route('register') }}"
+                                            >
+                                                Registrarse
+                                            </a>
+                                        @endif
+                                    @endauth
+                                @endif  
+                            </div>
+                        </div>
+                    </div>
         </section>
         <section class="cuadro_inferior">
             <div>
@@ -55,5 +78,52 @@
     </header>
     <!-- Línea divisoria -->
     <hr>
-</body>
+    <footer class="footer">
+            <div class="footer-contenedor">
+                <!-- Contacto -->
+                <div class="footer-col">
+                    <h3> CONTACTO </h3>
+                    <hr>
+                    <p> <strong> Teléfono fijo: </strong> 672 0380</p>
+                    <p> <strong> Línea de atención: </strong>  (+57) 316 671 2526</p>
+                    <p> <strong> Whatsapp ventas: </strong>  (+57) 316 671 2526</p>
+                    <p> <strong> Dirección: </strong> Cra. 13 # 65 - 10 Bogotá</p>
+                    <p> <strong> Email: </strong> contacto@lasuperbodega.com</p>
+                    <p> <strong> Horario: </strong> Lunes a Sábado de 9:00 am - 6:00 pm</p>
+                </div>
+
+                <!-- Información -->
+                <div class="footer-col">
+                    <h3>INFORMACIÓN</h3>
+                    <hr>
+                    <p><a href="#">Quiénes somos</a></p>
+                    <p><a href="#">Términos y condiciones</a></p>
+                    <p><a href="#">Política de privacidad</a></p>
+                    <p><a href="#">Preguntas frecuentes</a></p>
+                    <p><a href="#">Contacto</a></p>
+                </div>
+
+                <!-- Mi cuenta -->
+                <div class="footer-col">
+                    <h3>MI CUENTA</h3>
+                    <hr>
+                    <p><a href="#">Acceder / Registrarse</a></p>
+                    <p><a href="#">Recuperar / Cambiar contraseña</a></p>
+                    <p><a href="#">Mis pedidos</a></p>
+                    <p><a href="#">Seguimiento de envíos</a></p>
+                    <p><a href="#">Aceptación de términos y condiciones</a></p>
+                </div>
+
+                <!-- Síguenos -->
+                <div class="footer-col">
+                    <h3>SÍGUENOS</h3>
+                    <hr>
+                    <div class="footer-redes">
+                        <a href="#"><img src="{{asset('Img/Instagram (2).png')}}" alt="Instagram" /></a>
+                        <a href="#"><img src="{{asset('Img/Facebook (2).png')}}" alt="Facebook" /></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
 </html>
